73738dfca850911b2afc74ff202caee419852a22
Joy Neop <joyneop@gmail.com>
2014-10-27 13:15:48 +0000

    God's in his heaven. All's right with the world.

--- main.js
+++ main.js
@@ -48,3 +48,9 @@ var submitEmailPreorder = function () {
 	};
 };
 */
+
+var showCoreTeamMembers = function () {
+	document.getElementById("core-team").style.height = "196px";
+	document.getElementById("core-team").style.paddingTop = "20px";
+	document.getElementById("core-team").style.paddingBottom = "20px";
+};
